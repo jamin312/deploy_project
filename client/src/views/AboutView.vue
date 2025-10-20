7ab7c13b741fcab208f568dccd9e545f972bf78b
Jeong JaeMin <jamin312@naver.com>
2025-10-20 11:26:44 +0900

    JM_deploy_251020

---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <br />
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref, onBeforeMount } from "vue";
+
+const title = ref("");
+
+const getTitle = async () => {
+  let res = await axios.get("/api/board").catch(console.log);
+  title.value = res.data.title;
+  console.log(res.data);
+};
+
+onBeforeMount(() => {
+  getTitle();
+});
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/board", (req, res) => {
+  res.send({ title: "노드 api 서버 update!!" });
+});
+
+const path = require("path");
+const publicPath = path.join(__dirname, "public");
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
+});
+
+// 미들웨어로 등록한 거임  라우팅 아님
+// 에러처리 => index.html을 응답 => vue-router가 알아서 처리
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "./public", "index.html"));
+});
