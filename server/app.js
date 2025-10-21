79b3398681224c6624fc7d31afa056edd9fd7007
Jeong JaeMin <jamin312@naver.com>
2025-10-21 09:36:24 +0900

    JM/deploy Test/251021

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/api/board", (req, res) => {
-  res.send({ title: "노드 api 서버 update!!" });
+  res.send({ title: "Build Deploy Test" });
 });
 
 const path = require("path");
